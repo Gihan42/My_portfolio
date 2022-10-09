bd14dc84eccaadac1d6f3f56b17daa7edb373e51
Gihan42 <gihanm729@gmail.com>
2022-10-09 22:09:30 +0530

    git commit

---
+++ assigmnent/js/spa/controlers/CustomerControlers.js
@@ -0,0 +1,262 @@
+
+var customers=[]
+$('#save-customer').click(function(){
+    saveCustomer();
+    loadAllCustomers();
+    loadDetails();
+    doubleClickRemoveRow();
+    clear();
+    alert('savecustomer btn')
+})
+function saveCustomer(){
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Your work has been saved',
+        showConfirmButton: false,
+        timer: 1500
+    })
+    let customerId=  $('#txtcustomerId').val()
+    let custonerName=  $('#txtcustomerName').val()
+    let customerAddress=  $('#txtcustomerAddress').val()
+    let customerSalary=  $('#txtcustomerSalary').val()
+
+    var custome = {
+        customerId,
+        custonerName,
+        customerAddress,
+        customerSalary
+    }
+
+    customers.push(custome)
+    console.log(customers)
+}
+$('#getAll-customer').click(function(){
+    loadAllCustomers();
+    clear()
+})
+
+function loadAllCustomers(){
+    $('#customer-tablebody').empty()
+    for(var i of customers){
+        // console.log('customerId='+i.customerId)
+        var row=`<tr ><td>${i.customerId}</td><td >${ i.custonerName}</td> <td >${i.customerAddress}</td><td >${i.customerSalary}</td></tr>`;
+        $('#customer-tablebody').append(row);
+    }
+}
+// SEARCH CUSTOMER
+$('#search').click(function(){
+    let typedId = $("#customer-search").val();
+    let customer = searchCustomer(typedId);
+    if (customer != null) {
+        $('#txtcustomerId').val(customer.customerId)
+        $('#txtcustomerName').val(customer.custonerName)
+        $('#txtcustomerAddress').val(customer.customerAddress)
+        $('#txtcustomerSalary').val(customer.customerSalary)
+    } else {
+        Swal.fire("There is no cusotmer available for that " + typedId)
+        setTextfieldValues("", "", "", "");
+    }
+})
+function searchCustomer(cusID) {
+    for (let customer of customers) {
+        if (customer.customerId == cusID) {
+            return customer;
+            console.log(customer)
+        }
+    }
+    return null;
+}
+//END SEARCH CUSTOMER
+
+//////CUSTOMER DELETE//////
+$('#customerDelete').click(function(){
+    let cusId=$("#customer-search").val();
+    let option=confirm('Do you want to delete this customer'+cusId)
+    if(option){
+        if(deleteCustomer(cusId)){
+            Swal.fire({
+                icon: 'delete',
+                title: 'Deleted...',
+                text: 'Customer Successfully Deleted!',
+            })
+            $('#txtcustomerId').val('')
+            $('#txtcustomerName').val('')
+            $('#txtcustomerAddress').val('')
+            $('#txtcustomerSalary').val('')
+        }
+        else{
+            Swal.fire("No such customer to delete. please check the id" )
+        }
+    }
+
+})
+function deleteCustomer(customerID){
+    let customer= searchCustomer(customerID)
+    if(customer!=null){
+        let indexNo= customers.indexOf(customer);
+        customers.splice(indexNo,1)
+        loadAllCustomers()
+        return true;
+    }else{
+        return false;
+    }
+}
+//////END   CUSTOMER DELETE//////
+/////UPDATE CUSTOMER//////
+$('#customerUpdate').click(function(){
+    let cutomerId=$('#txtcustomerId').val()
+    let response=updateCustomer(cutomerId)
+    if(response){
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Customer Updated Successfully',
+            showConfirmButton: false,
+            timer: 1500
+        })
+        $('#txtcustomerId').val('')
+        $('#txtcustomerName').val('')
+        $('#txtcustomerAddress').val('')
+        $('#txtcustomerSalary').val('')
+    }
+    else{
+        Swal.fire("Update Failed..!" )
+    }
+})
+function updateCustomer(customerID){
+    let customer= searchCustomer(customerID)
+    if(customer !=null){
+        customer.customerID=$('#txtcustomerId').val()
+        customer.custonerName= $('#txtcustomerName').val()
+        customer.customerAddress= $('#txtcustomerAddress').val()
+        customer.customerSalary= $('#txtcustomerSalary').val()
+        loadAllCustomers()
+        return true
+    }
+    else{
+        return false
+    }
+}
+////END UPDATE CUSTOMER////
+function clear(){
+    $('#txtcustomerId').val('')
+    $('#txtcustomerName').val('')
+    $('#txtcustomerAddress').val('')
+    $('#txtcustomerSalary').val('')
+}
+////////end find customer////////
+//CLICK ROW AND SET VALUES TEXT FEILD//////
+$('#loadForm').click(function(){
+    loadDetails()
+})
+function loadDetails(){
+    $("#customer-tablebody>tr").click(function(){
+        let id= $(this).children(':eq(0)').text()
+        let name=$(this).children(':eq(1)').text()
+        let address=$(this).children(':eq(2)').text()
+        let salary=$(this).children(':eq(3)').text()
+        console.log(id,name,address,salary)
+
+        $('#txtcustomerId').val(id)
+        $('#txtcustomerName').val(name)
+        $('#txtcustomerAddress').val(address)
+        $('#txtcustomerSalary').val(salary)
+    })
+}
+///// DOUBLE CLICK REMOVE ROW /////
+function removeRow(){
+    $("#customer-tablebody>tr").on('dblclick',function(){
+        $(this).remove()
+    });
+}
+///// END REMOVE ROW /////
+$('#addnewCustomer').click(function(){
+    $('#txtcustomerId').focus()
+})
+///ENTER EVENT  SAVE CUSTOMER & VALIDATION///
+$('#txtcustomerId').focus()
+$('#txtcustomerId').on('keydown',function(event){
+    if(event.key=='Enter'){
+        var id = /^(C)[0-9]{3}$/;
+        var result = id.test($("#txtcustomerId").val());
+        if (result) {
+            $("#txtcustomerId").css({
+                'border': '2px solid green'
+            })
+            $('#txtcustomerName').focus();
+        } else {
+            $("#txtcustomerId").css({
+                'border-color': 'red'
+            })
+
+            $('#txtcustomerId').error='Customer ID Pattern is Wrong : C00-001'
+        }
+    }
+})
+$('#txtcustomerName').on('keydown',function(event){
+    if(event.key=='Enter'){
+        var Name = /^[A-z ]{3,15}$/;
+        var result = Name.test($("#txtcustomerName").val());
+        console.log(result);
+
+        if (result) {
+            $("#txtcustomerName").css({
+                'border-color': 'green'
+            })
+            $('#txtcustomerAddress').focus();
+        } else {
+            $("#txtcustomerName").css({
+                'border-color': 'red'
+            })
+        }
+    }
+})
+$('#txtcustomerAddress').on('keydown',function(event){
+    if(event.key=='Enter'){
+        var address=/^[A-z0-9 ,/]{4,20}$/
+        var result=address.test($('#txtcustomerAddress').val())
+        if(result){
+            //no3,Galle
+            $('#txtcustomerAddress').css({
+                'border-color': 'green'
+            })
+            $('#txtcustomerSalary').focus();
+        }
+        else{
+            $('#txtcustomerAddress').css({
+                'border-color': 'red'
+            })
+        }
+    }
+})
+$('#txtcustomerSalary').on('keydown',function(event){
+    if(event.key=='Enter'){
+        var salary=/^[1-9][0-9]*(.[0-9]{2})?$/;
+        var result=salary.test($("#txtcustomerSalary").val())
+        if (result){
+            $('#txtcustomerSalary').css({
+                'border-color': 'green'
+            })
+            confirm('Do you want to save this customer')
+            saveCustomer();
+            loadAllCustomers();
+            clear();
+        }
+        else{
+            $('#txtcustomerSalary').css({
+                'border-color': 'red'
+            })
+        }
+    }
+})
+
+////disable tab key of all  text fields
+$('#txtcustomerId,#txtcustomerName,#txtcustomerAddress,#txtcustomerSalary').on('keydown',function(event){
+    if(event.key=='Tab'){
+        event.preventDefault();
+    }
+})
+$('#SaveChange').click(function(){
+    alert('ok')
+})
---
+++ assigmnent/js/spa/controlers/ItemControllers.js
@@ -0,0 +1,253 @@
+var itemsArray=[];
+$('#save-item').click(function(){
+  saveItem();
+  getAllItem();
+  loadDetails();
+  clear()
+})
+function saveItem(){
+Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Your item has been saved',
+    showConfirmButton: false,
+    timer: 1500
+})
+  let itemcode= $('#txtItemcode').val();
+  let itemName= $('#txtItemName').val();
+  let itemPrice= $('#txtItemPrice').val();
+  let itemqty= $('#txtitemQty').val();
+
+  var item={
+    itemcode,
+    itemName,
+    itemPrice,
+    itemqty
+  }
+  itemsArray.push(item)
+  console.log(itemsArray)
+}
+$('#getAll-iteam').click(function(){
+  getAllItem();
+})
+function getAllItem(){
+  $('#iteam-tablebody').empty();
+  for(var i of itemsArray ){
+  var row=`<tr ><td>${i.itemcode}</td><td >${ i.itemName}</td> <td >${i.itemPrice}</td><td >${i.itemqty}</td></tr>`;
+  $('#iteam-tablebody').append(row)
+}
+clear()
+}
+// SEARCH ITEM
+// function findItem(){
+//   var result=$('#item-search').val();
+//   for (var i of itemsArray){
+//     if(i.itemcode == result){
+//       $('#txtItemcode').val(i.itemcode)
+//       $('#txtItemName').val(i.itemName)
+//       $('#txtItemPrice').val(i.itemPrice)
+//       $('#txtitemQty').val(i.itemqty)
+//     }
+//   }
+// }
+function searchItem(itemCode){
+  for (let item of itemsArray) {
+    if(item.itemcode==itemCode){
+        return item;
+    }
+  }
+  return null;
+}
+$('#search').click(function(){
+let typecode=$('#item-search').val();
+let item=searchItem(typecode)
+if(item !=null){
+$('#txtItemcode').val(item.itemcode)
+$('#txtItemName').val(item.itemName)
+$('#txtItemPrice').val(item.itemPrice)
+$('#txtitemQty').val(item.itemqty)
+}
+else {
+    Swal.fire("There is no item available for that " + typecode)
+    }
+})
+$('#txtItemcode').change(function(){
+findItem()
+})
+function clear(){
+$('#txtItemcode').val('')
+$('#txtItemName').val('')
+$('#txtItemPrice').val('')
+$('#txtitemQty').val('')
+}
+//////////// end find item ///////////
+
+////ITEM DELETE /////
+$('#delet-item').click(function(){
+let iCode=$('#item-search').val();
+let option=confirm('Do you want to delete this item'+iCode)
+if(option){
+if(deletItem(iCode)){
+  Swal.fire({
+    icon: 'delete',
+    title: 'Deleted...',
+    text: 'Item Successfully Deleted!',
+})
+$('#txtcustomerId').val('')
+$('#txtcustomerName').val('')
+$('#txtcustomerAddress').val('')
+$('#txtcustomerSalary').val('')
+
+}
+else{
+Swal.fire("No such item to delete. please check the id" )
+}
+}
+})
+function deletItem(itemcode){
+let item=searchItem(itemcode)
+if(item!=null){
+    let indexNo=itemsArray.indexOf(item)
+    itemsArray.splice(indexNo,1)
+    getAllItem();
+    return true;
+}
+else{
+  return false;
+}
+
+}
+
+///END ITEM DELETE////
+///UPDATE ITEM////
+$('#update-item').click(function(){
+let itemCode=$('#txtItemcode').val()
+let response=updateItem(itemCode)
+if(response){
+  Swal.fire({
+position: 'center',
+icon: 'success',
+title: 'Customer Updated Successfully',
+showConfirmButton: false,
+timer: 1500
+})
+$('#txtItemcode').val('')
+$('#txtItemName').val('')
+$('#txtItemPrice').val('')
+$('#txtitemQty').val('')
+}
+else{
+  Swal.fire("Update Failed..!" )
+}
+})
+function updateItem(itemCode){
+let item=searchItem(itemCode)
+if(item != null){
+  item.itemCode=$('#txtItemcode').val()
+  item.itemName= $('#txtItemName').val()
+  item.itemPrice= $('#txtItemPrice').val()
+  item.itemqty=$('#txtitemQty').val()
+  getAllItem()
+  return true
+}
+else{
+  return false
+}
+}
+///DELETE ITEM///
+//CLICK ROW AND SET VALUES TEXT FEILD/////
+$('#loadForm').click(function(){
+loadDetails()
+})
+function loadDetails(){
+$("#iteam-tablebody>tr").click(function(){
+let code=$(this).children(':eq(0)').text()
+let name=$(this).children(':eq(1)').text()
+let price=$(this).children(':eq(2)').text()
+let qty=$(this).children(':eq(3)').text()
+
+$('#txtItemcode').val(code)
+$('#txtItemName').val(name)
+$('#txtItemPrice').val(price)
+$('#txtitemQty').val(qty)
+})
+}
+///ENTER EVENT  SAVE ITEM  & VALIDATION///
+$('#txtItemcode').on('keydown',function(event){
+if(event.key=='Enter'){
+var code=/^(I)[0-9]{3}$/;
+var result=code.test($('#txtItemcode').val());
+if(result){
+  $("#txtItemcode").css({
+    'border-color': 'green'
+  })
+  $('#txtItemName').focus();
+}
+else{
+  $("#txtItemcode").css({
+    'border-color': 'red'
+  })
+}
+}
+})
+$('#txtItemName').on('keydown',function(event){
+if(event.key=='Enter'){
+var Name=/^[A-z ]{3,15}$/
+var result=Name.test($('#txtItemName').val());
+if(result){
+  $("#txtItemName").css({
+    'border-color': 'green'
+  })
+  $('#txtItemPrice').focus();
+}
+else{
+  $("#txtItemName").css({
+    'border-color': 'red'
+  })
+}
+
+}
+})
+$('#txtItemPrice').on('keydown',function(event){
+if(event.key=='Enter'){
+var price=/^[1-9][0-9]*(.[0-9]{2})?$/;
+var result=price.test($('#txtItemPrice').val());
+if(result){
+  $("#txtItemPrice").css({
+    'border-color': 'green'
+  })
+  $('#txtitemQty').focus();
+}
+else{
+  $("#txtItemPrice").css({
+    'border-color': 'red'
+  })
+}
+}
+})
+$('#txtitemQty').on('keydown',function(event){
+if(event.key=='Enter'){
+var qty=/^[0-9]{1,}$/;
+var result=qty.test($('#txtitemQty').val());
+if(result){
+  $("#txtitemQty").css({
+    'border-color': 'green'
+  })
+confirm('Do you want to save this item');
+saveItem();
+getAllItem();
+}
+else{
+  $("#txtitemQty").css({
+    'border-color': 'red'
+  })
+}
+
+}
+})
+////disable tab key of all  text fields
+$('#txtItemcode,#txtItemName,#txtItemPrice,#txtitemQty').on('keydown',function(event){
+ if(event.key=='Tab'){
+  event.preventDefault();
+ }
+})
